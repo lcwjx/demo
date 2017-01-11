d1cfa775aed34115a484cecfecde7186280ce8b8
李晨 <13585269669@163.com>
2017-01-11 14:22:30 +0800

    commit

---
+++ app/src/main/java/com/zn/lichen/lcdemo/view/activity/MainActivity.java
@@ -0,0 +1,59 @@
+package com.zn.lichen.lcdemo.view.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.zn.lichen.framework.base.BaseActivity;
+import com.zn.lichen.lcdemo.R;
+import com.zn.lichen.lcdemo.view.fragment.TabFragment;
+import com.zn.lichen.lcdemo.view.fragment.TabTwoFragment;
+import com.zn.lichen.lcdemo.widget.FragmentTabHost;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.tabhost)
+    FragmentTabHost mTabhost;
+
+    private final String TAB_TAG1 = "tab_tag1";
+    private final String TAB_TAG2 = "tab_tag2";
+    private final String TAB_TAG3 = "tab_tag3";
+
+    @Override
+    protected String[] listReceiveActions() {
+        return new String[0];
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        mTabhost.addTab(TAB_TAG1, TabFragment.class, null, R.id.tab_1);
+        mTabhost.addTab(TAB_TAG2, TabTwoFragment.class, null, R.id.tab_2);
+        mTabhost.addTab(TAB_TAG3, TabFragment.class, null, R.id.tab_3);
+        mTabhost.setUp(this, getSupportFragmentManager());
+        mTabhost.setTabHostListener(new FragmentTabHost.TabHostListener() {
+            @Override
+            public void onTabChange(int postion, String tag) {
+
+            }
+
+            @Override
+            public boolean onTabClick(String tag) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+    }
+}
---
+++ app/src/main/java/com/zn/lichen/lcdemo/view/fragment/TabFragment.java
@@ -0,0 +1,51 @@
+package com.zn.lichen.lcdemo.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.zn.lichen.framework.base.BaseFragment;
+import com.zn.lichen.lcdemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by lichen on 2017/1/11.
+ */
+
+public class TabFragment extends BaseFragment {
+    @BindView(R.id.content_text)
+    TextView mContentText;
+
+    private Unbinder mUnbinder;
+
+    @Override
+    protected String[] listReceiveActions() {
+        return new String[0];
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_layout, null);
+        mUnbinder = ButterKnife.bind(this, root);
+        mContentText.setText("TabFragment");
+        return root;
+    }
+
+    @Override
+    public void onDestroy() {
+        mUnbinder.unbind();
+        super.onDestroy();
+    }
+}
